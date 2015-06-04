c7ea8d2e8259f22e0ec91f36a5f343f142c4c3b5
ruiz93 <jpabloruizr@gmail.com>
2015-06-04 17:01:43 -0600

    Tablas JP
    Ya agregué mis tablas y un parentesis que hacía falta.

--- SQL tablas Bases Proyecto.sql
+++ SQL tablas Bases Proyecto.sql
@@ -1,5 +1,3 @@
-
-
 ---tablas proyecto bases II.
 
 create table poblado(
@@ -19,7 +17,7 @@ create table Region(
 id int      identity (1,1),
 nombre      varchar(30) not null,
 viviendas_O int,
-geometria   geometry check(geometria.STGeometryType = 'MultiPolygon') not null,
+geometria   geometry check(geometria.STGeometryType() = 'MultiPolygon') not null,
 
 constraint regionPK primary key(id),
 );
@@ -46,6 +44,43 @@ cantidad  int check(cantidad > 0),
 
 constraint infraestructuraPK primary key(id_Canton),
 constraint infraestructuraFK foreign key(id_Canton) references Canton(id)
+);
+
+------Tablas JP------
+
+create table Canton (
+	id int, 
+	nombre varchar(30),
+	pob_200h int,
+	pob_200m int, 
+	areacanton int, 
+	geometria geometry, 
+	idprovinciaFK int,
+	
+	constraint cantonPK primary key(id),
+	constraint cantonFK foreign key (idprovinciaFK) references Provincia (id) on delete cascade on update cascade
+);
+
+create table GeneracionElectrica (
+	id int not null,
+	nombre varchar(30),
+	empresa varchar(30),
+	tipo varchar(30),
+	capacidad int check(capacidad >= 0),
+	propiedad varchar(30),
+	estado varchar(30),
+	geometria geometry check(geometria.STGeometryType() = 'Point') not null,
+	
+	constraint GeneracionElectricaPK primary key(id)
+);
+
+create table Edades (
+	id_RegionFK int not null,
+	tipo varchar(30),
+	cantidad int check(cantidad >= 0),
+	
+	constraint EdadesPK primary key(id_RegionFK),
+	constraint EdadesFK foreign key (id_RegionFK) references Region (id) on delete cascade on update cascade
 );
 
 
